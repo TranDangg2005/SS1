37cae44c791f23792818ff47a0b6296170100d93
TranDangg2005 <147477825+TranDangg2005@users.noreply.github.com>
2023-11-08 09:17:30 +0700

    Add files via upload

---
+++ D.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+	//1.Khai bao va khoi tao canh hinh vuong,chieu dai chieu rong hcn
+	int canh_hv;
+	int canh_dai;
+	int chieu_rong;
+	//2.Ban kinh duong tron co kieu du lieu Float
+	float bk_hinh_tron;
+	//3.Khai bao hang so Pi kieu du lieu float
+	const float PI=3.14;
+	//4.Tinh chu vi dien tich hinh vuong
+	int cv_hinh_vuong=(canh_hv*4);
+	int cv_hcn=((canh_dai+chieu_rong)*2);
+	int cv_ht=(2*PI*bk_hinh_tron);
+	}
